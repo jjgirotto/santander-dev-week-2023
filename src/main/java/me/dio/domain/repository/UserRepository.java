d9ac147b9918a9610974b2e802539af72b526df4
Juliana Girotto <ads.jjgirotto@gmail.com>
2023-10-19 22:53:52 -0300

    Initial commit

---
+++ src/main/java/me/dio/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package me.dio.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import me.dio.domain.model.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByAccountNumber(String accountNumber);
+
+}
